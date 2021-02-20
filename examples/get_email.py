b76cf1130e6c50c8dda19fa9f51ebd7a4f2d97f4
William Zhang <jollychang@gmail.com>
2021-02-20 10:18:21 +0800

    print location
    print location

--- examples/get_email.py
+++ examples/get_email.py
@@ -2,10 +2,10 @@ from github_email_explorer import github_email
 
 ges = github_email.collect_email_info('yuecen', 'github-email-explorer', ['star', 'watch'])
 
-for ge in ges:
-    print ge.g_id, "->", ge.name, ",", ge.email
-
 # With Authentication
 # github_api_auth = ('<your_client_id>', '<your_client_secret>')
 # ges = github_email.collect_email_info('yuecen', 'github-email-explorer', ['star', 'watch'],
 #                                        github_api_auth=github_api_auth)
+
+for ge in ges:
+    print ge.g_id, "->", ge.name, ",", ge.email, ",", ge.location
